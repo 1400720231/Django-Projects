14bc17781db4f4904fb807c8f29d06db22a00c90
panda <18778331181@sina.cn>
2018-10-22 13:50:09 +0800

    删除mysite文件夹，单独拉出来作项目

--- mysite/blog/__init__.py
+++
@@ -1,3 +0,0 @@
-# 关于app的相关配置文件指定
-
-default_app_config = "blog.apps.BlogConfig"  
--- mysite/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	# 博客标题视图函数
-	url(r'^$', views.blog_title, name='blog_title'),
-
-]
